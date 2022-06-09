98d0386ea68488d48da2f5906903337688cfdfbe
hwanjuson <hwanjuson@gmail.com>
2022-06-09 11:41:16 +0900

    머충 ui및 ui세팅관련 코드

--- ZepetoGGMProject/Assets/01.Scripts/UI/ExhibitPanel.ts
+++ ZepetoGGMProject/Assets/01.Scripts/UI/ExhibitPanel.ts
@@ -1,5 +1,7 @@
-import { CanvasGroup } from 'UnityEngine';
+import { CanvasGroup, GameObject, Transform,Sprite } from 'UnityEngine';
+import { Image, Text,Button } from 'UnityEngine.UI';
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import UIVO from '../VO/UIVO';
 
 export default class ExhibitPanel extends ZepetoScriptBehaviour {
 
@@ -7,16 +9,56 @@ export default class ExhibitPanel extends ZepetoScriptBehaviour {
 
     private cvs: CanvasGroup;
 
+    @SerializeField()
+    private gameMainImg: Image;
+    @SerializeField()
+    private bannerImg: Image;
+
+    private bannerSprites: Sprite[];
+
+    @SerializeField()
+    private buttonParent: Transform;
+
+    private buttonList: Button[];
+
+    @SerializeField()
+    private gameNameText: Text;
+    @SerializeField()
+    private gameExplanationText: Text;
+    @SerializeField()
+    private madeByText: Text;
+
     Awake() {
         this.cvs = this.GetComponent<CanvasGroup>();
 
+        if (ExhibitPanel.Instance != null) {
+            GameObject.Destroy(this.gameObject);
+        }
         ExhibitPanel.Instance = this;
     }
 
     Start() {
-
+        this.buttonList = this.buttonParent.GetComponentsInChildren<Button>();
     }
 
+    SetUI(uiVO: UIVO) {
+        this.gameMainImg.sprite = uiVO.gameMainSprite;
+        this.bannerSprites = uiVO.bannerSprite;
+        this.bannerImg.sprite = this.bannerSprites[0];
+        this.gameNameText.text = uiVO.gameName;
+        this.gameExplanationText.text = uiVO.gameExplanation;
+        this.madeByText.text = uiVO.madeBy;
+
+
+        for (var i = 0; i < this.buttonList.length; i++) {
+            var j = i;
+                
+            this.buttonList[j].onClick.RemoveAllListeners();
+            this.buttonList[j].onClick.AddListener(() => {
+                this.bannerImg.sprite = this.bannerSprites[j];
+            });
+        }
+    }
 
     Open() {
         this.OpenOrClose(true);
---
+++ ZepetoGGMProject/Assets/01.Scripts/VO/UIVO.ts
@@ -0,0 +1,9 @@
+import { Sprite } from 'UnityEngine'
+
+export default class UIVO {
+    public gameMainSprite: Sprite;
+    public bannerSprite: Sprite[];
+    public gameName: string;
+    public gameExplanation: string;
+    public madeBy: string;
+}
