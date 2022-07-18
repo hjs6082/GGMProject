9369ff43e52dd5265a1beca37c0b4f2776151d00
sunwoong-m <datanet231@gmail.com>
2022-07-18 12:50:33 +0900

    시점 및 기타 작업

--- ZepetoGGMProject/Assets/01.Scripts/Woong/InSchool.ts
+++ ZepetoGGMProject/Assets/01.Scripts/Woong/InSchool.ts
@@ -4,7 +4,7 @@ import { Button, Image } from 'UnityEngine.UI';
 import { LocalPlayer, ZepetoPlayer, ZepetoPlayers } from 'ZEPETO.Character.Controller';
 
 export default class InSchool extends ZepetoScriptBehaviour {
-    @Header("Teleport ê´€ë ¨")
+    @Header("Teleport ê´?? ¨")
     public Floor_1_Trm:Transform;
     private player:LocalPlayer;
 
@@ -12,11 +12,13 @@ export default class InSchool extends ZepetoScriptBehaviour {
         this.player = ZepetoPlayers.instance.LocalPlayer;
 
         this.player.zepetoPlayer.character.Teleport(stair.position, Quaternion.identity);
+
+        ZepetoPlayers.instance.cameraData.minZoomDistance = 0.2;
+        ZepetoPlayers.instance.cameraData.maxZoomDistance = 0.5;
     }
 
     OnTriggerEnter(cols:Collision) {
         console.log("ajkladfjkla");
-
 
         this.Teleport(this.Floor_1_Trm);
     }
